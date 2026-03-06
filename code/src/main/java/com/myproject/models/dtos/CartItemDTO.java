f5900f50825e09edd914f27de2e7fa07ebfef54a
loginkarun <loginkarun@gmail.com>
2026-03-06 11:58:30 +0530

    Auto commit: Scripts committed on 2026-03-06-06-28-30

--- code/src/main/java/com/myproject/models/dtos/CartItemDTO.java
+++ code/src/main/java/com/myproject/models/dtos/CartItemDTO.java
@@ -1,20 +1,16 @@
 package com.myproject.models.dtos;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class CartItemDTO {
 
-    private Long id;
     private Long productId;
-    private String productName;
-    private Integer quantity;
+    private String name;
     private Double price;
-    private Double subtotal;
+    private Integer quantity;
 }
