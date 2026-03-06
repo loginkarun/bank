905a96d46eb1dbccac00e0c125dadb2caf63c5a6
loginkarun <loginkarun@gmail.com>
2026-03-06 11:59:32 +0530

    Auto commit: Scripts committed on 2026-03-06-06-29-32

--- code/src/main/java/com/myproject/exceptions/OutOfStockException.java
+++ code/src/main/java/com/myproject/exceptions/OutOfStockException.java
@@ -1,13 +1,12 @@
 package com.myproject.exceptions;
 
 public class OutOfStockException extends RuntimeException {
-    
+
     public OutOfStockException(String message) {
         super(message);
     }
-    
-    public OutOfStockException(Long productId, Integer requestedQuantity, Integer availableStock) {
-        super(String.format("Product %d is out of stock. Requested: %d, Available: %d", 
-                productId, requestedQuantity, availableStock));
+
+    public OutOfStockException(Long productId) {
+        super("Product is out of stock: " + productId);
     }
 }
