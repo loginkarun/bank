66103f0d74025ca03ce23bfc4065c3f6fde04aa3
loginkarun <loginkarun@gmail.com>
2026-03-06 12:00:56 +0530

    Auto commit: Scripts committed on 2026-03-06-06-30-55

--- code/src/main/java/com/myproject/services/interfaces/CartService.java
+++ code/src/main/java/com/myproject/services/interfaces/CartService.java
@@ -1,18 +1,17 @@
 package com.myproject.services.interfaces;
 
 import com.myproject.models.dtos.CartItemAddRequest;
+import com.myproject.models.dtos.CartItemRemoveRequest;
+import com.myproject.models.dtos.CartItemUpdateRequest;
 import com.myproject.models.dtos.CartResponse;
-import com.myproject.models.dtos.UpdateQuantityRequest;
 
 public interface CartService {
-    
-    CartResponse addToCart(CartItemAddRequest request, Long userId);
-    
+
+    CartResponse addItemToCart(Long userId, CartItemAddRequest request);
+
     CartResponse getCart(Long userId);
-    
-    CartResponse removeFromCart(Long productId, Long userId);
-    
-    CartResponse updateQuantity(Long productId, UpdateQuantityRequest request, Long userId);
-    
-    CartResponse clearCart(Long userId);
+
+    CartResponse removeItemFromCart(Long userId, CartItemRemoveRequest request);
+
+    CartResponse updateItemQuantity(Long userId, CartItemUpdateRequest request);
 }
