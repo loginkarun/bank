a4cb409fd3eb781a6b0e933a799b1df0cfff4880
loginkarun <loginkarun@gmail.com>
2026-03-06 11:59:27 +0530

    Auto commit: Scripts committed on 2026-03-06-06-29-26

--- code/src/main/java/com/myproject/exceptions/ProductNotFoundException.java
+++ code/src/main/java/com/myproject/exceptions/ProductNotFoundException.java
@@ -1,11 +1,11 @@
 package com.myproject.exceptions;
 
 public class ProductNotFoundException extends RuntimeException {
-    
+
     public ProductNotFoundException(String message) {
         super(message);
     }
-    
+
     public ProductNotFoundException(Long productId) {
         super("Product not found with ID: " + productId);
     }
