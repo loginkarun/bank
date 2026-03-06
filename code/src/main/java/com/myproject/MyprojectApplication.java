4ac06a834c48fc7256b3bc0cd12abf528d2118cb
loginkarun <loginkarun@gmail.com>
2026-03-06 11:53:14 +0530

    Auto commit: Scripts committed on 2026-03-06-06-23-13

---
+++ code/src/main/java/com/myproject/MyprojectApplication.java
@@ -0,0 +1,14 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.myproject.models.repositories")
+public class MyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyprojectApplication.class, args);
+    }
+}
