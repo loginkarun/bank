1cd44881bee7a623ecbbedb0c02891661d1a1d1f
loginkarun <loginkarun@gmail.com>
2026-03-06 11:58:42 +0530

    Auto commit: Scripts committed on 2026-03-06-06-28-42

--- code/src/main/java/com/myproject/models/dtos/ErrorResponse.java
+++ code/src/main/java/com/myproject/models/dtos/ErrorResponse.java
@@ -1,7 +1,6 @@
 package com.myproject.models.dtos;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,14 +11,18 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class ErrorResponse {
 
     private LocalDateTime timestamp;
     private String traceId;
     private String errorCode;
     private String message;
-    
-    @Builder.Default
     private List<ErrorDetail> details = new ArrayList<>();
+
+    public ErrorResponse(LocalDateTime timestamp, String traceId, String errorCode, String message) {
+        this.timestamp = timestamp;
+        this.traceId = traceId;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
 }
